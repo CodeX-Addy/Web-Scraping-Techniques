0aaf2bbc8220e386974155a09bbeca2e8a0c0c6e
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2024-12-03 10:35:27 +0530

    bs4

---
+++ BeautifulSoup_Test.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+def beautiful_soup_web_scraping(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return str(soup)
+
+url = "https://medium.com/kx-systems/rag-llamaparse-advanced-pdf-parsing-for-retrieval-c393ab29891b"
+
+data=beautiful_soup_web_scraping(url)
+print(data)
